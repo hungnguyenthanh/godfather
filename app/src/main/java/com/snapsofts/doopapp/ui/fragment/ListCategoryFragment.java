e263dd775ccf6d83ba91b46cf4e8c56c58da608b
HungNT <hungnguyen.soict@gmail.com>
2016-04-22 00:17:26 +0700

    Adding Swipe To Dissmiss

--- app/src/main/java/com/snapsofts/doopapp/ui/fragment/ListCategoryFragment.java
+++ app/src/main/java/com/snapsofts/doopapp/ui/fragment/ListCategoryFragment.java
@@ -3,32 +3,21 @@ package com.snapsofts.doopapp.ui.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.snapsofts.doopapp.R;
-import com.snapsofts.doopapp.data.model.Category;
 import com.snapsofts.doopapp.ui.view.DividerItemDecoration;
-import com.snapsofts.doopapp.ui.view.VerticalSpaceItemDecoration;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
 public class ListCategoryFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
-    private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
+    private int mListType = 1;
     private static final String ARG_SECTION_NUMBER = "section_number";
+    private CategoryRecyclerViewAdapter mAdapter;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -37,8 +26,6 @@ public class ListCategoryFragment extends Fragment {
     public ListCategoryFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
     public static ListCategoryFragment newInstance(int sectionNumber) {
         ListCategoryFragment fragment = new ListCategoryFragment();
         Bundle args = new Bundle();
@@ -52,7 +39,7 @@ public class ListCategoryFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
         if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+            mListType = getArguments().getInt(ARG_SECTION_NUMBER);
         }
     }
 
@@ -68,40 +55,27 @@ public class ListCategoryFragment extends Fragment {
 
         recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), R.drawable.divider_horizontal));
 
-        recyclerView.setAdapter(new CategoryRecyclerViewAdapter());
+
+        mAdapter = new CategoryRecyclerViewAdapter();
+
+        recyclerView.setAdapter(mAdapter);
+
+
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
         return view;
     }
 
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        if (context instanceof OnListFragmentInteractionListener) {
-//            mListener = (OnListFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnListFragmentInteractionListener");
-//        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(Category item);
-    }
 }
