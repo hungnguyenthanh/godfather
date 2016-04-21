9589db1b182a1f5215802418213f0238275a744a
HungNT <hungnguyen.soict@gmail.com>
2016-04-22 03:19:04 +0700

    Update ListView Choose Category

--- app/src/main/java/com/snapsofts/doopapp/ui/activity/HomeActivity.java
+++ app/src/main/java/com/snapsofts/doopapp/ui/activity/HomeActivity.java
@@ -2,10 +2,16 @@ package com.snapsofts.doopapp.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.snapsofts.doopapp.R;
 import com.snapsofts.doopapp.data.model.User;
+import com.snapsofts.doopapp.util.SwipeGestureDetector;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,6 +21,9 @@ public class HomeActivity extends BaseActivity {
 
     @Bind(R.id.btnWishList)
     TextView btnWishList;
+    @Bind(R.id.layoutView)
+    FrameLayout layoutView;
+    private GestureDetector mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,26 @@ public class HomeActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final Animation exitAnimation = AnimationUtils.loadAnimation(this, R.anim.exit_to_top);
+
+        mDetector = new GestureDetector(this, new SwipeGestureDetector(
+                new SwipeGestureDetector.OnSwipeListener() {
+                    @Override
+                    public void onSwipe(int slope) {
+                        switch (slope) {
+                            case SwipeGestureDetector.TOP:
+                                layoutView.startAnimation(exitAnimation);
+                                break;
+                            case SwipeGestureDetector.DOWN:
+                                break;
+                        }
+                    }
+                }));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mDetector.onTouchEvent(event);
     }
 
     @Override
