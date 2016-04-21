468d50e53b7b030694e9615f302ecc975399d835
HungNT <hungnguyen.soict@gmail.com>
2016-04-22 01:18:21 +0700

    Update

--- app/src/main/java/com/snapsofts/doopapp/ui/activity/HomeActivity.java
+++ app/src/main/java/com/snapsofts/doopapp/ui/activity/HomeActivity.java
@@ -1,26 +1,28 @@
 package com.snapsofts.doopapp.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
 
 import com.snapsofts.doopapp.R;
 import com.snapsofts.doopapp.data.model.User;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class HomeActivity extends BaseActivity {
 
+    @Bind(R.id.btnWishList)
+    TextView btnWishList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_coupon);
+        setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
     }
 
@@ -28,12 +30,13 @@ public class HomeActivity extends BaseActivity {
     protected void buttonHomeClick() {
     }
 
-    @OnClick(R.id.btnUserMenu)
+    @OnClick(R.id.btnAdd)
     public void onClick() {
         if (!User.userLoggedIn(getApplicationContext())) { //User not logged in => Show login screen.
             gotoLogin();
         } else {
             //TODO - Add this item to wishlist
+
         }
     }
 }
